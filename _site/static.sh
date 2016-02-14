7b27b46224d22d30021a5b4c3e5903eae787f8c0
John Firebaugh <john.firebaugh@gmail.com>
2016-02-14 09:50:25 -0800

    Convert to metalsmith

--- _site/static.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mv ~/Downloads/$1.svg static/$1.svg
-bg=$(sed -nE 's/.*style="background: ([^;]+);".*/\1/p' static/$1.svg)
-inkscape --export-png=static/$1.png --export-area-page --export-background="$bg" static/$1.svg
---
+++ build.js
@@ -0,0 +1,62 @@
+var Handlebars = require('handlebars');
+var Metalsmith = require('metalsmith');
+var date = require('metalsmith-date-in-filename');
+var paths = require('metalsmith-paths');
+var collections = require('metalsmith-collections');
+var permalinks = require('metalsmith-permalinks');
+var layouts = require('metalsmith-layouts');
+var pagination = require('metalsmith-pagination');
+var assets = require('metalsmith-assets');
+
+Handlebars.registerHelper('each-reverse', require('diy-handlebars-helpers/lib/each-reverse'));
+
+Metalsmith(__dirname)
+  .source('posts')
+  .metadata({
+    title: 'Animations',
+    baseurl: '/animations'
+  })
+  .use(date({
+    override: true
+  }))
+  .use(paths({
+    property: 'paths'
+  }))
+  .use(collections({
+    posts: '**'
+  }))
+  .use(function (files, metalsmith, done) {
+    Object.keys(files).forEach(function (name) {
+      if (/\.frag$/.test(name)) {
+        var data = files[name];
+        data.layout = 'glsl.html';
+        delete files[name];
+        files[name.replace(/\.frag$/, '.html')] = data;
+      }
+    });
+    done();
+  })
+  .use(permalinks({
+    pattern: ':date',
+    date: 'YYYY-MM-DD'
+  }))
+  .use(pagination({
+    posts: {
+      perPage: 100,
+      path: ':num',
+      layout: 'index.html',
+      first: 'index.html',
+      noPageOne: true
+    }
+  }))
+  .use(layouts({
+    engine: 'handlebars',
+    default: 'default.html'
+  }))
+  .use(assets({
+    source: 'assets',
+    destination: '.'
+  }))
+  .build(function (err) {
+    if (err) throw err;
+  });
